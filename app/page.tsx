4e7dace84f3ff9644f314845548fa078bbfc38b8
Brandon Trump <brandontrump101@gmail.com>
2023-09-18 18:22:27 +1200

    feat: included the latest news section to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import AboutSection from "@/components/About/AboutSection";
 import ExploreSection from "@/components/Explore/ExploreSection";
 import FeatureSection from "@/components/Features/FeatureSection";
 import TestimonialSection from "@/components/Testimonials/TestimonialSection";
+import LatestNews from "@/components/LatestNews/LatestNews";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <ExploreSection />
       <FeatureSection />
       <TestimonialSection />
+      <LatestNews />
     </main>
   );
 }
