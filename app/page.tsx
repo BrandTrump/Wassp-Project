b565e1f63e684828288a1a402ca520648ce1eaa8
Brandon Trump <brandontrump101@gmail.com>
2023-09-19 00:50:42 +1200

    refactor: removed unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import styles from "./page.module.css";
 import HeroSection from "@/components/Hero/HeroSection";
-import Container from "@mui/material/Container";
-import NewsArticle from "@/components/NewsArticle/NewsArticle";
 import AboutSection from "@/components/About/AboutSection";
 import ExploreSection from "@/components/Explore/ExploreSection";
 import FeatureSection from "@/components/Features/FeatureSection";
