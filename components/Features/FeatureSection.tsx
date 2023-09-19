1aba5728d70bc0cdc7396e79930c0889307ae12d
Brandon Trump <brandontrump101@gmail.com>
2023-09-19 17:22:04 +1200

    refactor: moved the feature information outside of the feature component

--- components/Features/FeatureSection.tsx
+++ components/Features/FeatureSection.tsx
@@ -3,65 +3,7 @@ import React, { useState } from "react";
 import styles from "./FeaturesSection.module.scss";
 import Image from "next/image";
 import { Container } from "@mui/material";
-
-const features = [
-  {
-    id: 1,
-    number: "01",
-    title: "High Resolution Chirp Sounder",
-    content:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, ipsum.",
-  },
-  {
-    id: 2,
-    number: "02",
-    title: "120 side to side coverage",
-    content:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, ipsum.",
-  },
-  {
-    id: 3,
-    number: "03",
-    title: "Realtime CDX Mapping",
-    content:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, ipsum.",
-  },
-  {
-    id: 4,
-    number: "04",
-    title: "Realtime 3D View CDX",
-    content:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, ipsum.",
-  },
-  {
-    id: 5,
-    number: "05",
-    title: "Backscatter license",
-    content:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, ipsum.",
-  },
-  {
-    id: 6,
-    number: "06",
-    title: "1. High Resolution Side Scan",
-    content:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, ipsum.",
-  },
-  {
-    id: 7,
-    number: "07",
-    title: "Third-Party Software Intergration",
-    content:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, ipsum.",
-  },
-  {
-    id: 8,
-    number: "08",
-    title: "Multiple WASSP Transducers",
-    content:
-      "Lorem ipsum dolor sit amet consectetur. Arcu commodo nibh elementum facilisis tellus. Sit malesuada volutpat ut eget. Quis purus commodo enim auctor libero malesuada. Ornare facilisis pharetra tortor mi ac elementum leo sed mi. Quis dui proin mi ipsum enim.",
-  },
-];
+import { features } from "@/data/data";
 
 function FeatureSection() {
   const [selected, setSelected] = useState(1);
---
+++ data/data.ts
@@ -0,0 +1,58 @@
+export const features = [
+  {
+    id: 1,
+    number: "01",
+    title: "High Resolution Chirp Sounder",
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, ipsum.",
+  },
+  {
+    id: 2,
+    number: "02",
+    title: "120 side to side coverage",
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, ipsum.",
+  },
+  {
+    id: 3,
+    number: "03",
+    title: "Realtime CDX Mapping",
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, ipsum.",
+  },
+  {
+    id: 4,
+    number: "04",
+    title: "Realtime 3D View CDX",
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, ipsum.",
+  },
+  {
+    id: 5,
+    number: "05",
+    title: "Backscatter license",
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, ipsum.",
+  },
+  {
+    id: 6,
+    number: "06",
+    title: "1. High Resolution Side Scan",
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, ipsum.",
+  },
+  {
+    id: 7,
+    number: "07",
+    title: "Third-Party Software Intergration",
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, ipsum.",
+  },
+  {
+    id: 8,
+    number: "08",
+    title: "Multiple WASSP Transducers",
+    content:
+      "Lorem ipsum dolor sit amet consectetur. Arcu commodo nibh elementum facilisis tellus. Sit malesuada volutpat ut eget. Quis purus commodo enim auctor libero malesuada. Ornare facilisis pharetra tortor mi ac elementum leo sed mi. Quis dui proin mi ipsum enim.",
+  },
+];
